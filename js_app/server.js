c1de41643c584f3ba770e200d57b383b6f4680c3
viz <kafkafranz495@gmail.com>
2025-03-26 18:59:51 -0400

    js chat room

---
+++ js_app/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const {Server} = require('socket.io')
+
+const app = express()
+app.use(express.static('public'))
+const es = app.listen(4000)
+
+const io = new Server(es,{
+	cors: ['http://localhost:4000']
+})
+
+io.on(
+	'connection', socket=>{
+		console.log(socket.id, " has joined")
+		socket.on("msgCtoS", data=>{
+			io.emit('msgStoAllC', data)
+			}
+		)
+	}
+)
